63f4f9ec98d9c78c4439fc4c7d92318cd10dafb7
Devin Catuns <devincatuns1@gmail.com>
2025-10-15 20:29:08 -0400

    added authentication auto configuration

---
+++ jwt-auth/src/main/java/xyz/catuns/spring/jwt/auth/properties/JwtAuthProperties.java
@@ -0,0 +1,25 @@
+package xyz.catuns.spring.jwt.auth.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+/**
+ * Jwt Authentication manager properties
+ *
+ * @author Devin Catuns
+ * @since 1.0.0
+ */
+@Data
+@ConfigurationProperties(prefix = "jwt.auth")
+public class JwtAuthProperties {
+
+    /**
+     * Enables authentication manager
+     *
+     */
+    private boolean enabled = true;
+    /**
+     * Enable use of UserEntityService
+     */
+    private boolean useEntityService = true;
+}
